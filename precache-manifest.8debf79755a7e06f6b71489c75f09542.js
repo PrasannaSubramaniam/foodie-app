a5b3811e3906d7007a6a4c1b5c8b658223973d28
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-06-30 16:24:18 +0530

    Updates

---
+++ precache-manifest.8debf79755a7e06f6b71489c75f09542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113f934509ce569cee591cb832c9ae47",
+    "url": "/foodie-app/index.html"
+  },
+  {
+    "revision": "b5e5276914d2a4a49c04",
+    "url": "/foodie-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79c46bbe8bda7e901dbc",
+    "url": "/foodie-app/static/js/2.a7fb85c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodie-app/static/js/2.a7fb85c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e5276914d2a4a49c04",
+    "url": "/foodie-app/static/js/main.7ffb27ab.chunk.js"
+  },
+  {
+    "revision": "b6fd4434ee2dc0949e19",
+    "url": "/foodie-app/static/js/runtime-main.ed10dac2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/foodie-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
